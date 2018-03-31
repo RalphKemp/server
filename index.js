17ab59d913d610b658fa5019211a70ec706e51bc
ralph kemp <ralphk3mp@gmail.com>
2018-03-31 22:22:35 +0100

    still working on server

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there'});
 }); // this is a route handler. the current method is get, and can take other api requests like post, delete etc.
 
-app.listen(5000); // express telling node which port to listen to.
+const PORT = process.env.PORT || 5000;
+// when heroku runs, it injects environment variables.
+// Look at the underliying enviroment, and see if we've stated a port to use
+
+app.listen(PORT); // express telling node which port to listen to.
