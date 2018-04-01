aae35384f0f5c39321ecbbc24c130aa947458d5d
ralph kemp <ralphk3mp@gmail.com>
2018-04-01 01:22:23 +0100

    got google clinet id and secret

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express'); // common js module
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there'});
-}); // this is a route handler. the current method is get, and can take other api requests like post, delete etc.
+passport.use(new GoogleStrategy());
+
+// client id: client_id":"548233704598-smqt5rck2gc5o8du8fo9a5d08h4qitdd.apps.googleusercontent.com
+// client secret: yZ4mBitdrCrzwD-CG9453nNE
+
 
 const PORT = process.env.PORT || 5000;
 // when heroku runs, it injects environment variables.
