5ebbeae7d29a803e540b257db04a963ea948cd33
ralph kemp <ralphk3mp@gmail.com>
2018-04-07 23:59:00 +0100

    added new route handler

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express'); // common js module
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken) => {
+    console.log(accessToken);
+  })
+);
+
+// GoogleStrategy says to passport: hey if anyone tries to authenticate with a string called google,
+// (like below in the route handler, then use me). the scope specifies to google what access we want to have
+// of the users account. You could also have like contacts, or see their images etc, basically just different
+// permissions.
+
+
+app.get('/auth.google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
 
 
 const PORT = process.env.PORT || 5000;
@@ -12,3 +33,5 @@ const PORT = process.env.PORT || 5000;
 // Look at the underliying enviroment, and see if we've stated a port to use
 
 app.listen(PORT); // express telling node which port to listen to.
+
+
