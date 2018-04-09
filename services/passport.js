8b6bafa3a4052df64ba2c4fa7194a721a44d009a
ralph kemp <ralphk3mp@gmail.com>
2018-04-09 21:34:44 +0100

    added serialize and notes

--- services/passport.js
+++ services/passport.js
@@ -1,9 +1,21 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
 const mongoose = require('mongoose');
+const keys = require('../config/keys');
 
 const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user,id);
+});
+
+// profile id and user id are different.
+// user id is a shortcut to _id in mongoDB
+
+
+
+
+
 // now user is our model class (collection)
 
 passport.use(
@@ -11,20 +23,29 @@ passport.use(
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-  }, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id})
-      .then((existingUser) => {
-        if (existingUser) {
+  },
+  (accessToken, refreshToken, profile, done) => {
+    User.findOne({ googleId: profile.id}).then(existingUser => {
+      if (existingUser) {
           // we already have a record with thegiven profile id
+          done(null, existingUser);
         } else {
           // we don't have a record, make a new one
-          new User({ googleId: profile.id }).save();
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+          // the second instance of user here represents the same instance, but by convention we make
+          // use of the one provided to us inside of the promise callback.
         }
-      })
-    // any time we reach out to our mongoose db, we are initiating an asyncronous action.
-    // the query returns a promise.
-  })
+      });
+    }
+  )
 );
+
+ // any time we reach out to our mongoose db, we are initiating an asyncronous action.
+    // the query returns a promise.
+    // 'exisiting user' he obviously is a model instance.
+    // 'done' is a function that tells passport that we're done.
 
 // above, .save on the new user is persisting the new class instance to our database.
 
