2d750cd95f8a89b2a1c9dc2e63bd6464b9ea7e50
ralph kemp <ralphk3mp@gmail.com>
2018-04-25 01:39:04 +0100

    formatting

--- client/src/index.js
+++ client/src/index.js
@@ -10,11 +10,19 @@ const store = createStore(() => [], {}, applyMiddleware());
 ReactDOM.render(
   <Provider store={store}><App /></Provider>,
   document.querySelector('#root')
+);
+
+
+
+
+
+
+
+
+
 // Here we've created a redux store at the very top level of our application,
 // and hooked it up to the react side of our application by placing
 // the provider tag. Provider is a react component that knows how to read changes
 // from our redux store any time it gets some new state produced inside of it,
 // - the porvider will then inform of all of it's children components (everything in App.js)
 // then will update them all with the new state.
-
-);
