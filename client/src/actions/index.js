52a45f1a096d5c1653d73624b60712d3e8fe4a76
ralph kemp <ralphk3mp@gmail.com>
2018-05-27 16:22:40 +0100

    installed eslint, turned app into a class based component to add lifecycle method - currently console logging the actions comnig through but api request not working. debugging now.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,21 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser = () => {
-  axios.get('/api/current_user');
+export const fetchUser = () => {
+  return function(dispatch) {
+    axios
+      .get('/api/current_user')
+      .then(res => dispatch({ type: FETCH_USER, payload: res}));
+  };
 };
+
+
+
+// redux thunk breaks the rule that we have to return an action
+// from every action creator. It basically gives us access to the dispatch function.
+// allowing us to manually dispatch an action instead of just returning it.
+
+// if redux thunk sees that we return a function instead of a normal action,
+// it will automatically call the function and pass in the dispatch function into it.
+// we can now say when we want to dispatch the action - so when we get a succsessul response from
+// our api, then we can say 'ok i want to dispatch the action now'.
