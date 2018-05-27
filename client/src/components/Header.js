c79869c9cb71e71427ac71bd08cdb1e59abb79f5
ralph kemp <ralphk3mp@gmail.com>
2018-05-27 22:19:50 +0100

    got basic login header functionality working, user can now see if they are logged in or out. For this created rendercontent function with switch statement

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Login With Google</a></li>;
+      default:
+        return <li><a>Logout</a></li>;
+    }
+  }
+
+
   render() {
+    console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">
             Emaily
           </a>
-          <a className="right sign-in">
-            Login With Google
-          </a>
+          <ul className="right">
+            {this.renderContent()}
+          </ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({auth}) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
