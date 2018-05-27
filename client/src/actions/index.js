6f889e63e21f2c52a0e382aa32c1ec7b00e29282
ralph kemp <ralphk3mp@gmail.com>
2018-05-27 17:16:50 +0100

    added to auth reducer, console log of res.data shows user model, moving on to actually having that logic in the header component

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,17 +3,8 @@ import { FETCH_USER } from './types';
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
-  dispatch({ type: FETCH_USER, payload: res });
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
-
-
-
-
-
-
-
-
-
 
 
 // redux thunk breaks the rule that we have to return an action
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,14 @@
-export default function(state = {}, action) {
-  console.log(action);
+import { FETCH_USER } from '../actions/types';
+
+export default function(state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false; // res.data from action.
     default:
     return state;
   }
 }
+
+
+// default is null.
+// so our auth piece of state is always going to be either null, res.data(user model), or false.
