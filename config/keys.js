4053cc51d93da3112f4458cd51a0efbabb28cc30
ralph kemp <ralphk3mp@gmail.com>
2018-06-03 20:09:04 +0100

    added stripe payments button and handle token action that will return the user model, with an updated number of credits which will be put in the header

--- config/keys.js
+++ config/keys.js
@@ -7,16 +7,3 @@ if (process.env.NODE_ENV === 'production') {
   // development
   module.exports = require('./dev');
 }
-
-
-
-
-
-// google production keys
-
-// client 898332218166-2c80p0h7e38mimt3nedllu0fcitbr7rt.apps.googleusercontent.com
-
-// secret 5GmskQUF0upejZq8hUDWNOzN
-
-
-// mongo: mongodb://ralphkemp:Downfalls2323@ds143774.mlab.com:43774/emaily-ralph-prod
