b6f7bffe698213457eb9dd16b6fbb7a87a5862d3
ralph kemp <ralphk3mp@gmail.com>
2018-06-04 23:18:23 +0100

    added logic in index js for production routing problems

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 // app.use. = they are wiring up to middleware. They are small functions that can modify
 // incoming requests to our app become they are sent off to route handlers.
 // we can assign data to the cookie, cookie-session as a middleware then takes all the data
@@ -31,6 +30,16 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets like our main.js file, or main.cookie-session
+  app.use(express.static('client/build'));
+  // Express will serve up the index.html file if it doesnt recognize the route
+  const path = require('path');
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+  });
+}
 
 // when we require the authRoutes file, it returns a function.
 // we then immedaitly call that function with the app object.
