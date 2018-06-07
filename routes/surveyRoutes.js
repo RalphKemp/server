749066240eb06eabf68204622265da1e001fe432
ralph kemp <ralphk3mp@gmail.com>
2018-06-07 23:09:04 +0100

    working on survey creation

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,17 +1,24 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const checkBalance = require('../middlewares/checkBalance');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
   app.post('/api/surveys', requireLogin, checkBalance, (req, res) => {
-    if (req.user.credits >= 1) {
-      res.send
-    }
+    const { title, subject, body, recipients } = req.body;
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email }))
+    });
   });
 };
 
 
 
-
-
 // requireLogin here is a reference to a function, we're not calling it directly.
 // We're saying hey, if there is a req and res, here's a request to the function that you'll run
+
+
