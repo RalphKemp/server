ac262270f3654c5047034af357adeee8f58f3ecb
ralph kemp <ralphk3mp@gmail.com>
2018-06-09 18:09:40 +0100

    added sendgrid and started to set up Mailer class

--- config/prod.js
+++ config/prod.js
@@ -6,7 +6,8 @@ module.exports = {
   MONGODB_URI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY
 };
 
 // these environment varibles are set on heroku
