6a356e7ba983cbd0c4122400bed7994cccd8e676
ralph kemp <ralphk3mp@gmail.com>
2018-06-10 19:22:41 +0100

    created mailer / sendgrid

--- services/Mailer.js
+++ services/Mailer.js
@@ -4,10 +4,44 @@ const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
-class Mailer extends help.Mail {
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
 
+    this.from_email = new helper.Email('no-reply@emailyyyy.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({email}) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Peronalization();
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
 }
 
+module.exports = Mailer;
 
 // we are extending the mail class from the sendgrid library.
 // now are class mailer contains a bunch of setup which is inherited from our help.mail.
