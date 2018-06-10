41f404bb7ba62f91320fe9e3532c257ba90e3acb
ralph kemp <ralphk3mp@gmail.com>
2018-06-10 19:27:27 +0100

    sending off mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,5 +1,4 @@
 // Mailer.js starts with a capital letter becuase we're exporting a class
-
 const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
 const keys = require('../config/keys');
@@ -8,6 +7,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email('no-reply@emailyyyy.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
@@ -38,6 +38,17 @@ class Mailer extends helper.Mail {
       personalize.addTo(recipient);
     });
     this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: 'this.toJSON()'
+    });
+
+    const response = this.sgApi.API(request);
+    return response;
   }
 }
 
