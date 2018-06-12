ef381b4da8f922255c6f5e29d213de77999a9435
ralph kemp <ralphk3mp@gmail.com>
2018-06-12 23:47:36 +0100

    starting surveynew component

---
+++ client/src/components/SurveyNew.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SurveyNew extends Component {
+  render() {
+    return(
+      <div>
+        <p>new survey</p>
+      </div>
+    );
+  }
+}
+
+export default SurveyNew;
