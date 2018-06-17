5d7fd41481ec72599ee169ef37adf881f241fb88
ralph kemp <ralphk3mp@gmail.com>
2018-06-17 19:18:50 +0100

    added survey field component and rendering it in survey new

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 // redux form function here similiar to connect function, connecting form to application state
+import SurveyField from './SurveyField';
 
 class SurveyForm extends Component {
+  renderFields() {
+    return(
+      <div>
+        <Field type="text" name="title" component={SurveyField}/>
+      </div>
+    );
+  }
+
   render() {
     return(
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field type="text"name="surveyTitle"component="input"/>
+          {this.renderFields()}
           <button type="submit">Submit</button>
         </form>
       </div>
@@ -21,5 +30,5 @@ export default reduxForm({
 
 
 // 'name' on the field is the name of the javascript object (who's value is the text inputed)
-// that will be stored in the redux store. 
-// our arrow function will inside the handleSubmit will be called whenever a user submits the form. 
+// that will be stored in the redux store.
+// our arrow function will inside the handleSubmit will be called whenever a user submits the form.
