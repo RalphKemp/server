e35e12002fd72318a6dcdeed8763975361701d56
ralph kemp <ralphk3mp@gmail.com>
2018-06-17 21:08:32 +0100

    used lodash to iterate over fields array

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,15 +1,21 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 // redux form function here similiar to connect function, connecting form to application state
 import SurveyField from './SurveyField';
 
+const FIELDS = [
+  { label: 'Survey Title', name: 'title'},
+  { label: 'Subject Line', name: 'subject'},
+  { label: 'Email body', name: 'body'},
+  { label: 'Recipient List', name: 'emails'}
+]
+
 class SurveyForm extends Component {
   renderFields() {
-    return(
-      <div>
-        <Field type="text" name="title" component={SurveyField}/>
-      </div>
-    );
+    return _.map(FIELDS, ({ label, name }) => {
+      return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+    });
   }
 
   render() {
@@ -32,3 +38,7 @@ export default reduxForm({
 // 'name' on the field is the name of the javascript object (who's value is the text inputed)
 // that will be stored in the redux store.
 // our arrow function will inside the handleSubmit will be called whenever a user submits the form.
+
+// adding on label as a prop to the field, it is automatically passed on to our survey field component.
+
+// note: keys for an iteration have to be consistent across renders.
