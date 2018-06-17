d8058209fc376acb26df01772f6295fd626278ca
ralph kemp <ralphk3mp@gmail.com>
2018-06-17 20:22:10 +0100

    updated survey field input

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const SurveyField = () => {
+const SurveyField = ({ input }) => {
   return(
     <div>
-      <input />
+      <input {...input} />
     </div>
   );
 }
 
 export default SurveyField;
+
+// the ... says, we've got a big old object with a lot of props,
+// we want the input to have all the keys and values in it. equivilint of writing
+// out onBlur={input.onBlur}, onchange={input.onChange} etc...
+
+// the es6 input argument at the top looks on to the props object, and pulls of the input property
+// and assigns it to a variable called input.
