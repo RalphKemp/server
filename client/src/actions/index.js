09b8a47a27c85b882a53d43890b124413f5f69d2
ralph kemp <ralphk3mp@gmail.com>
2018-07-01 18:40:48 +0100

    created survey list component with action crator and reducer to render surveys into the component, and then call that component in the dashboard

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import swal from 'sweetalert';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
@@ -19,7 +19,12 @@ export const submitSurvey = (values, history) => async dispatch => {
   history.push('/surveys');
   swal('survey sent');
   dispatch({ type: FETCH_USER, payload: res.data});
-}
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+  dispatch({ type: FETCH_SURVEYS, payload: res.data});
+};
 
 // redux thunk breaks the rule that we have to return an action
 // from every action creator. It basically gives us access to the dispatch function.
